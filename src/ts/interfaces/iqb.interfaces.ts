c2a59a6d6f11b869bfb0640d8c5d65e4c08f3eb2
paflov <philipp.franck@hu-berlin.de>
2025-04-28 14:22:25 +0200

    Npm package 0.0.8

--- src/ts/interfaces/iqb.interfaces.ts
+++ src/ts/interfaces/iqb.interfaces.ts
@@ -1,9 +1,5 @@
 import {VariableCodingData} from "@iqbspecs/coding-scheme/coding-scheme.interface";
 
-export interface Codes {
-  [code: string]: number
-}
-
 export const ResponseStatusList = [
     'UNSET',
     'NOT_REACHED',
